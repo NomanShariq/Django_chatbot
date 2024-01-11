43154b5f16ff264565c67f8266ca5c3a8bf6706c
Syed Noman Shariq <nshariq562@gmail.com>
2024-01-12 03:20:25 +0500

    Javascript added and some templates added

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'Hi this is my response',
+        return JsonResponse({'message':message , 'response' : response})
     return render(request, 'chatbot.html')
